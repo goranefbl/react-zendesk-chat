351b06a2335f9d270e461cb24c50ddb8597da545
Goran Jakovljevic <goranefbl@gmail.com>
2019-01-15 00:00:43 +0100

    webpack setup update, optimized package

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,28 @@
+const path = require('path');
+
 module.exports = {
   entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2', // important, tells webpack not to pack dependendcies, and load them from parent.
+  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+          },
         },
       },
     ],
+  },
+  externals: {
+    react: 'commonjs react', // important, tells webpack not to pack dependendcies, and load them from parent.
   },
 };
